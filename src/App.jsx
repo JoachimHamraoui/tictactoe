e2efe4bbfa2f267c3328bacc5d4f079b30a2b6d2
JoachimHamraoui <joachim.hamraoui@student.ehb.be>
2023-12-07 13:52:15 +0100

    feature(game): added board with boxes and click function

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import './App.css'
+import { Board } from "./components/Board/Board"
+import {useState} from 'react'
 
 function App() {
 
+  // const board = ["X", "X", "X", "X", "X", "X", "X", "X", "X"]
+  const [board, setBoard] = useState(Array(9).fill(null));
+
+  const handleBoxClick = (boxIdx) => {
+    const updatedBoard = board.map((value, idx) => {
+      if (idx === boxIdx) {
+        return "X";
+      } else {
+        return value
+      }
+    })
+
+    setBoard(updatedBoard)
+  }
+
   return (
     <>
-      
+      <Board board={board} onClick={handleBoxClick} />
     </>
   )
 }
